d654f22e0902da8d1fb161c0a3f7a4f09f08f82b
amankton <am.ankton@outlook.com>
2026-03-25 14:41:19 -0700

    fix: Lenis scroll integration and hydration fixes

--- app/providers.jsx
+++ app/providers.jsx
@@ -1,9 +1,14 @@
 'use client'
 
+import React, { useEffect } from 'react'
 import { ReactLenis } from 'lenis/react'
 import 'lenis/dist/lenis.css'
 
 export default function Providers({ children }) {
+  React.useEffect(() => {
+    console.log("Lenis Providers mounted");
+  }, []);
+
   return (
     <ReactLenis
       root
